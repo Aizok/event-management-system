a7d28a806e8d4677828b4db0d4782b5d64d55423
Aizok <vladislavmishepud@gmail.com>
2026-04-10 17:34:10 +0300

    Создал task-service, чтобы исправить циклические импорты

--- task-service/app/models/__init__.py
+++ task-service/app/models/__init__.py
@@ -1,5 +1,6 @@
 from .base import Base
 from .task import Task
 from .task_dependency import TaskDependency
+from .task_history import TaskHistory
 
-__all__ = ["Base", "Task", "TaskDependency"]
+__all__ = ["Base", "Task", "TaskDependency", "TaskHistory"]
